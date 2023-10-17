04a2505c9c7bcf3e0d10ed6ca223d22405e8ee18
Wowa <rimidalf@tutamail.com>
2023-10-17 16:53:26 +0200

    add to Do boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="toDo">
+      <h1>To Do</h1>
+      <section className="addItem">
+        <input
+          type="text"
+          name=""
+          id=""
+          className="taskInput"
+          placeholder="new task ..."
+        />
+        <button className="addButton">ADD</button>
+      </section>
+      <section className="queue" id="">
+        <input className="checkbox" id="" type="radio" />
+        <p className="task" id="">
+          Aufgabe 1
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <div className="buttonContainer" id="">
+          <button className="editButton" id="" type="">
+            EDIT
+          </button>
+          <button className="deleteButton" id="" type="">
+            DELETE
+          </button>
+        </div>
+      </section>
+    </div>
+    <div className="Done" id="">
+      <h1 className="toDoh1" id="">
+        Done
+      </h1>
+      <section className="finished" id="">
+        <p className="finishedtask" id="">
+          Aufgabe 5
+        </p>
+        <button className="donedeleteButton" id="" type="">
+          DELETE
+        </button>
+      </section>
+    </div>
+    <button className="clearAll" id="" type="">
+      CLEAR LIST
+    </button>
+  </>
   )
 }
-
-export default App
